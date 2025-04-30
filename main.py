5f46479f9d6cabe7200e59aaa0a677ece008a011
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2025-04-30 22:19:57 +0500

    chore: cleaned up main.py

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
-#!/usr/bin/env python3
-
 """
-Secure File Encryptor/Decryptor
+Secure File/Folder/Drive Encryptor/Decryptor
 - Uses AES-GCM with unique nonce per chunk
 - Includes chunk sequence validation
 - Password strength checking
 - Progress reporting
 - Secure file handling
+- Includes a GUI
+- Has Password strength checking
+- Provides Password Recovery Options:
+    - Seed Phrase
+    - Security Questions
+    - Hardware Token
 """
 
+import os
 import sys
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import QCoreApplication, Qt
 from gui import MainWindow
-
 from qt_material import apply_stylesheet
 
 if __name__ == "__main__":
     QCoreApplication.setAttribute(Qt.AA_DisableWindowContextHelpButton)
     app = QApplication(sys.argv)
-    
-    apply_stylesheet(app, theme="dark_teal.xml")
+
+    apply_stylesheet(app, theme="dark_blue.xml")
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
